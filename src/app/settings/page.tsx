81020f2689604c6429538c1d78dba19cabf9c2c6
Nilesh Surya <9d4nilesh@gmail.com>
2025-10-22 23:45:54 +0000

    REMOVE CASHBOOK ADD TRASNACTION FROM SETTING PAGE

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,13 +1,9 @@
 import SettingsClient from '@/components/settings/SettingsClient';
 import { getBooks } from '@/lib/data';
-import Header from '@/components/layout/Header';
 
 export default async function SettingsPage() {
   const books = await getBooks();
   return (
-    <>
-      <Header />
       <SettingsClient initialBooks={books} />
-    </>
   );
 }
